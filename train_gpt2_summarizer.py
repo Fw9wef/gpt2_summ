ff70453c8f4f6793377f7a4d0da24eb64758c545
Al <Sasha252VNS@gmail.com>
2021-03-17 11:52:07 +0300

    initial commit

--- train_gpt2_summarizer.py
+++ train_gpt2_summarizer.py
@@ -18,9 +18,9 @@ from tqdm import tnrange, tqdm
 from dataset import GPT21024Dataset 
 from utils import add_special_tokens, beam_search, generate_beam_sample, generate_sample, sample_seq, set_seed, top_k_top_p_filtering
 
-#from trl.gpt2 import GPT2HeadWithValueModel, respond_to_batch
-#from trl.ppo import PPOTrainer
-#from bleurt import score
+from trl.gpt2 import GPT2HeadWithValueModel, respond_to_batch
+from trl.ppo import PPOTrainer
+from bleurt import score
 
 bleurt_checkpoint = "../bleurt/bleurt/test_checkpoint"
 
@@ -134,7 +134,61 @@ def evaluate(args, model, eval_dataset, ignore_index, global_step=None):
             for key in sorted(result.keys()):
                 f.write('\n\n')
                 f.write("time = %s, %s = %s, step = %s\n" % (datetime.now().strftime("%d/%m/%Y %H:%M:%S"), key, str(result[key]), str(global_step)))
-    return result           
+    return result
+
+'''
+def rl_train(args, model, model_ref, scorer, tokenizer, train_dataset, valid_dataset, ignore_index):
+    writer = SummaryWriter('./logs')
+    train_sampler = RandomSampler(train_dataset)
+    train_dl = DataLoader(train_dataset, sampler=train_sampler, batch_size=args.batch_size,
+                          num_workers=args.num_workers)
+
+    loss_fct = CrossEntropyLoss(ignore_index=ignore_index)  # ignores padding token for loss calculation
+    optimizer = AdamW(model.parameters(), lr=args.lr)
+    scheduler = WarmupLinearSchedule(optimizer, 100, 80000)
+
+    global_step = 0
+    tr_loss, logging_loss = 0.0, 0.0
+    model.zero_grad()
+    train_iterator = tnrange(int(args.num_train_epochs), desc="Epoch")
+    set_seed(args)
+    for _ in train_iterator:
+        epoch_iterator = tqdm(train_dl, desc="Training")
+        for step, batch in enumerate(epoch_iterator):
+            inputs, labels = torch.tensor(batch['article']), torch.tensor(batch['article'])
+            inputs = inputs.to(args.device)
+            labels = labels.to(args.device)
+            model.train()
+            logits = model(inputs)[0]
+            idx = batch['sum_idx'].item()  # index of separator token
+            # only consider loss on reference summary just like seq2seq models
+            shift_logits = logits[..., idx:-1, :].contiguous()
+            shift_labels = labels[..., idx + 1:].contiguous()
+            loss = loss_fct(shift_logits.view(-1, shift_logits.size(-1)), shift_labels.view(-1))
+            loss = loss / args.gradient_accumulation_steps
+            loss.backward()
+            torch.nn.utils.clip_grad_norm_(model.parameters(), args.max_grad_norm)
+            tr_loss += loss.item()
+            if (step + 1) % args.gradient_accumulation_steps == 0:
+                optimizer.step()
+                scheduler.step()  # Update learning rate schedule
+                model.zero_grad()
+                global_step += 1
+                writer.add_scalar('lr', scheduler.get_lr()[0], global_step)
+                writer.add_scalar('loss', (tr_loss - logging_loss) / args.gradient_accumulation_steps, global_step)
+                logging_loss = tr_loss
+                print("loss:", loss.item(), end='\n\n')
+                if (step + 1) / args.gradient_accumulation_steps == 1.0:
+                    print('After 1st update: ', end='\n\n')
+                    generate_sample(valid_dataset, tokenizer, num=2, eval_step=False, device=args.device)
+
+            if (step + 1) % (10 * args.gradient_accumulation_steps) == 0:
+                results = evaluate(args, model, valid_dataset, ignore_index, global_step)
+                for key, value in results.items():
+                    writer.add_scalar('eval_{}'.format(key), value, global_step)
+                print('After', global_step + 1, 'updates: ', end='\n\n')
+                generate_sample(valid_dataset, tokenizer, num=2, eval_step=True, device=args.device)
+'''
 
 def main():
     parser = argparse.ArgumentParser()
@@ -144,8 +198,8 @@ def main():
     parser.add_argument("--gradient_accumulation_steps",default=32, type=int, required=True, help="gradient_accumulation_steps")
     parser.add_argument("--batch_size",default=1, type=int, required=True, help="batch_size")
     parser.add_argument("--num_workers",default=4, type=int, required=False, help="num of cpus available")
-    parser.add_argument("--device",default=0, required=False, help="torch.device object")
-    parser.add_argument("--num_train_epochs",default=1, type=int, required=True, help="no of epochs of training")
+    parser.add_argument("--device",default=-1, required=False, help="torch.device object")
+    parser.add_argument("--num_train_epochs",default=5, type=int, required=True, help="no of epochs of training")
     parser.add_argument("--output_dir",default='./output', type=str, required=True, help="path to save evaluation results")
     parser.add_argument("--model_dir",default='./weights', type=str, required=True, help="path to save trained model")
     parser.add_argument("--fp16",default=True, type=bool, required=False, help="whether to use 16-bit (mixed) precision (through NVIDIA apex) instead of 32-bit")
@@ -175,19 +229,19 @@ def main():
     print('total time: ', (time.time()-start)/60, " minutes", end='\n\n')
 
     print('Saving trained model...')
-    model_file = os.path.join(args['model_dir'], 'model_{}_data{}_trained_after_{}_epochs_only_sum_loss_ignr_pad.bin'.format(args['fp16_opt_level'],3000,args['num_train_epochs']))
-    config_file = os.path.join(args['model_dir'], 'config_{}_data{}_trained_after_{}_epochs_only_sum_loss_ignr_pad.json'.format(args['fp16_opt_level'],3000,args['num_train_epochs']))
+    model_file = os.path.join(args.model_dir, 'gpt2_summ.pth')
+    config_file = os.path.join(args.model_dir, 'gpt2_summ.json')
     torch.save(model.state_dict(), model_file)
     model.config.to_json_file(config_file)
 
 
     #scorer = score.BleurtScorer(bleurt_checkpoint)
-
-    #gpt2_model = GPT2HeadWithValueModel.from_pretrained('weights/')
-    #gpt2_model_ref = GPT2HeadWithValueModel.from_pretrained()
-    #gpt2_tokenizer = GPT2Tokenizer.from_pretrained()
-
-
+    #model = GPT2HeadWithValueModel.from_pretrained('weights/gpt2_summ.pth')
+    #model_ref = GPT2HeadWithValueModel.from_pretrained('weights/gpt2_summ.pth')
+    #
+    #start = time.time()
+    #rl_train(args, model, model_ref, scorer, tokenizer, train_data, valid_data, ignore_idx)
+    #print('total time: ', (time.time() - start) / 60, " minutes", end='\n\n')
 
 if __name__ == '__main__':
     main()
