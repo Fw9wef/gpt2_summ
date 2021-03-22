b698128b4108527670b98ca266a8d90d373c73b1
Al <Sasha252VNS@gmail.com>
2021-03-22 15:05:06 +0300

    qwe

--- dataset.py
+++ dataset.py
@@ -36,10 +36,9 @@ class GPT21024Dataset(Dataset):
         with open(file_name, 'r') as f:
             data = json.load(f)
         text = self.pad * 1024
-        mask = text
         content = data['article'] + self.tokenizer.encode(self.tokenizer.sep_token) + data['abstract']
         text[:len(content)] = content
         text = torch.tensor(text)
-        mask = torch.where(text == self.pad[0], 1, 0)
+        mask = torch.where(text == self.pad[0], 0, 1)
         sample = {'article': text, 'sum_idx': len(data['article']), 'attention_mask': mask}
         return sample
