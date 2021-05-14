d2501051e3243f51821762b87d00e5da39354053
Al <Sasha252VNS@gmail.com>
2021-05-14 13:19:43 +0300

    1

--- dataset.py
+++ dataset.py
@@ -56,7 +56,7 @@ class GPT21024Dataset(Dataset):
 
         content = data['article'] + self.tokenizer.encode(self.tokenizer.sep_token) + abstract
         text[:len(content)] = content
-        text = torch.tensor(text)
+        text = torch.Tensor(text)
         mask = torch.where(text == self.pad[0], torch.Tensor(0), torch.Tensor(1))
         sample = {'article': text, 'sum_idx': len(data['article']), 'attention_mask': mask}
         return sample
